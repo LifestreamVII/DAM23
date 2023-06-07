89efacae9dc6287475222224316552cdc8b30d56
Loïs L <36632834+LifestreamVII@users.noreply.github.com>
2023-06-07 15:58:13 +0200

    Add files via upload

---
+++ ui/src/App.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ const [data, setData] = useState(null);
+ const [loading, setLoading] = useState(true);
+ const [error, setError] = useState(null);
+
+ useEffect(() => {
+  fetch(`http://php/api/v1`)
+   .then((response) => console.log(response));
+ }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
