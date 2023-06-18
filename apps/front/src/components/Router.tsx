50545c60c2eb611896ea39a289fd55b1c3b6c7ad
Leo <leo.coyere94@gmail.com>
2023-06-18 10:28:58 +0200

    Router component

---
+++ apps/front/src/components/Router.tsx
@@ -0,0 +1,14 @@
+import {
+	BrowserRouter,
+	Switch,
+	Route
+} from 'react-router-dom';
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Switch>
+            </Switch>
+        </BrowserRouter>
+    )
+}
