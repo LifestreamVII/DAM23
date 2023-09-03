b94b1d66fcf14f1e6ef94742ef270093c6b48005
Leo <leo.coyere94@gmail.com>
2023-09-03 22:57:34 +0200

    Nouveau Input Select

--- ui/src/components/Input.jsx
+++ ui/src/components/Input.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react"
+import arrowGrey from '../assets/images/arrow-grey.svg'
 
-export default function Input({type, value, setValue, children}) {
+export default function Input({type, value, setValue, children, options, multiple}) {
 
-    const inputType = type !== 'textarea' && type !== 'file' ? 'default' : type
+    const types = ['textarea', 'file', 'select']
+    const inputType = types.includes(type) ? type : 'default'
 
     const view = {
         default: <DefaultInput type={type} value={value} setValue={setValue} label={children} />,
         textarea: <InputTextArea value={value} setValue={setValue} label={children} />,
-        file: <InputFile value={value} setValue={setValue} label={children} />
+        file: <InputFile value={value} setValue={setValue} label={children} />,
+        select: <InputSelect value={value} setValue={setValue} label={children} options={options} multiple={multiple}/>
     }[inputType];
 
     return view
@@ -59,6 +62,43 @@ function InputTextArea({value, setValue, label}) {
         <div className={`input input--textarea ${isFocused || value ? 'input--focused' : ''}`} >
             <textarea id={`input-${id}`} onChange={e => setValue(e.target.value)} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)}></textarea>
             <label htmlFor={id}>{label}</label>
+        </div>
+    )
+}
+
+function InputSelect({value, setValue, label, options, multiple}) {
+
+    const [isFocused, setIsFocused] = useState(false)
+
+    function toggleIsFocused(e) {
+        setIsFocused(!isFocused)
+    }
+
+    options = options || []
+
+    return (
+        <div className={`input input--select ${!isFocused ? 'input--closed' : ''}`}>
+            <button className="label input__item" onClick={toggleIsFocused} type="button">
+                <span className="label__name">{label} ({options.length})</span>
+                <img className="label__arrow" src={arrowGrey} alt="" />
+            </button>
+            <div className="options">
+                {options.length ?
+                    options.map((option, index) => 
+                        <button
+                            type="button"
+                            className={`option input__item ${
+                                multiple ?
+                                    value.includes(option.value) ? 'option--selected' : ''
+                                : value === option.value ? 'option--selected' : ''
+                            }`}
+                            key={index}
+                            onClick={() => setValue(option.value)}
+                        >{option.text}</button> 
+                    )
+                : <button type="button" className="option input__item">Aucun résultat</button>
+                }
+            </div>
         </div>
     )
 }
