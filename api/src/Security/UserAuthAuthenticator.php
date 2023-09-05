596543f3a34f06681b17053fecac9561ef769c0d
MihailSofroniev <98062952+MihailSofroniev@users.noreply.github.com>
2023-09-05 15:37:46 +0200

    jwt v2

--- api/src/Security/UserAuthAuthenticator.php
+++ api/src/Security/UserAuthAuthenticator.php
@@ -2,56 +2,70 @@
 
 namespace App\Security;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CredentialsInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordAuthenticatedToken;
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+use Symfony\Component\Security\Http\Logout\DefaultLogoutSuccessHandler;
 
-class UserAuthAuthenticator extends AbstractLoginFormAuthenticator
+class CustomAuthenticator extends AbstractAuthenticator implements LogoutSuccessHandlerInterface
 {
-    use TargetPathTrait;
+    private $security;
 
-    public const LOGIN_ROUTE = 'app_login';
-
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(Security $security)
     {
+        $this->security = $security;
     }
 
-    public function authenticate(Request $request): Passport
+    public function supports(Request $request): ?bool
     {
-        $username = $request->request->get('username', '');
-        $request->getSession()->set(Security::LAST_USERNAME, $username);
+        return $request->attributes->get('_route') === 'app_login' && $request->isMethod('POST');
+    }
 
-        return new Passport(
-            new UserBadge($username),
-            new PasswordCredentials($request->request->get('password', '')),
-            [
-                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-            ]
+    public function authenticate(Request $request): PassportInterface
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $username = $data['username'] ?? '';
+        $password = $data['password'] ?? '';
+
+        if (empty($username) || empty($password)) {
+            throw new CustomUserMessageAuthenticationException('Missing username or password.');
+        }
+
+        return new SelfValidatingPassport(
+            new UserPassport($username),
+            new PasswordCredentials($password)
         );
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    public function onAuthenticationSuccess(Request $request, CredentialsInterface $credentials): ?Response
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }
+        // You can customize the behavior after successful authentication here.
+        // For example, you can generate a JWT token and return it in the response.
+        // You can also redirect the user to a specific route.
 
-        // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_home'));
-        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new JsonResponse(['message' => 'Authentication successful']);
     }
 
-    protected function getLoginUrl(Request $request): string
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+        return new JsonResponse(['message' => $exception->getMessage()], Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function onLogoutSuccess(Request $request): ?Response
+    {
+        return new JsonResponse(['message' => 'Logout successful']);
     }
 }
+?>
