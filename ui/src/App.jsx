c2b57d32335b6461059c042ca10726fd8e2e7d73
Leo <leo.coyere94@gmail.com>
2023-09-06 11:05:48 +0200

    Update stale time React Query

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -13,6 +13,7 @@ export default function App() {
             queries: {
                 refetchOnWindowFocus: false,
                 retry: 1,
+                staleTime: 1000 * 60 * 5 
             },
         },
     })
